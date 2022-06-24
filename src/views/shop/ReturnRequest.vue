6f214ba784866d077474f714bff75f2a5802bc24
MoriiIF <MoriiIF_221B@yeah.net>
2022-06-25 04:16:05 +0800

    '上传代码'

--- src/views/shop/ReturnRequest.vue
+++ src/views/shop/ReturnRequest.vue
@@ -2,41 +2,58 @@
     <div class="returnRequest">
         <h3>退货申请</h3>
         <el-table :data="tableData" style="width: 100%">
-            <el-table-column label="商品" width="300">
+        <el-table-column label="订单号" width="200">
                 <template slot-scope="scope">
-                    <img :src="scope.row.productImg" alt="" width="100px" height="100px">
-                    <span>{{scope.row.name}}</span>
+                    <span>{{scope.row.orderId}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="商品图片" width="100">
+                <template slot-scope="scope">
+                    <img :src="scope.row.picture" alt="" width="100px" height="100px">
+                </template>
+            </el-table-column>
+            <el-table-column label="商品sku" width="200">
+                <template slot-scope="scope">
+                    <span>{{scope.row.sku}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="数量" width="50">
+                <template slot-scope="scope">
+                    <span>{{scope.row.count}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="价格" width="50">
+                <template slot-scope="scope">
+                    <span>{{scope.row.price}}</span>
                 </template>
             </el-table-column>
             <el-table-column label="买家" width="100">
                 <template slot-scope="scope">
-                    <span>{{scope.row.buyer}}</span>
+                    <span>{{scope.row.id}}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="买家ID" width="100">
+            <el-table-column label="购买时间" width="100">
                 <template slot-scope="scope">
-                    <span>{{scope.row.buyerID}}</span>
-                </template>
-            </el-table-column>
-            <el-table-column label="交易状态" width="130">
-                <template>
-                    <!-- <span>{{scope.row.tradingStatus}}</span> -->
-                    <span>申请退货</span>
+                    <span>{{scope.row.purchaseTime}}</span>
                 </template>
             </el-table-column>
             <el-table-column label="操作" width="150">
-                <el-button size="small" v-if="btnAgree" @click="agree">同意</el-button>
-                <el-button size="small" type="danger" v-if="btnRefuse" @click="dialogVisible = true">拒绝</el-button>
+                <template slot-scope="scope">
+                <el-button size="small" v-if="btnAgree" @click="agree(scope.row)">同意</el-button>
+                <el-button size="small" type="danger" v-if="btnRefuse" @click="refuse(scope.row)">拒绝</el-button>
                 <span v-if="waiting">已同意，等待买家处理</span>
                 <span v-if="reject">已驳回申请</span>
+                </template>
             </el-table-column>
         </el-table>
+
+
         <el-dialog title="请填写驳回申请的理由" :visible.sync="dialogVisible">
             <el-input type="textarea" :rows="5" placeholder="请输入驳回理由" v-model="reason">
             </el-input>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="(dialogVisible = false) && refuse">确 定</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
             </div>
         </el-dialog>
     </div>
@@ -57,45 +74,58 @@ export default{
         }
     },
     created() {
-        this.getReturn();
+        this.getOrderList()
     },
     methods: {
-        agree(){
+        agree(row){
             this.btnAgree = false;
             this.btnRefuse = false;
             this.waiting = true;
+            var url = 'http://49.232.81.174:8080/commodity/setOrderList'
+            this.axios.get(url, {
+                params: {
+                    orderId: row.orderId,
+                    status: 5
+                }
+            }).then(res => {
+                if(res.data['message'] == "操作成功"){
+                    console.log(res.data['data'])
+                }
+            })
         },
         refuse(){
             this.btnAgree = false;
             this.btnRefuse = false;
             this.reject = true;
+            this.dialogVisible = true;
+            var url = 'http://49.232.81.174:8080/commodity/setOrderList'
+            this.axios.get(url, {
+                params: {
+                    orderId: row.orderId,
+                    status: 3
+                }
+            }).then(res => {
+                if(res.data['message'] == "操作成功"){
+                    console.log(res.data['data'])
+                }
+            })
         },
-        getReturn(){
-            this.dataLoading = true;
-            import('@/mock/returnApplication.json').then((res) => {
-                this.tableData = res.data;
-                this.dataLoading = false;
-            });
+        getOrderList(){
+            var url = 'http://49.232.81.174:8080/commodity/getOrderList'
+            this.axios.get(url, {
+                params: {
+                    shop: localStorage.getItem('userId'),
+                    status: 4
+                }
+            }).then(res => {
+                if(res.data['message'] == "操作成功"){
+                    this.tableData = res.data['data']
+                }
+            })
         }
     }
 }
-</script>
 
-<style lang="scss" scoped>
-
-</style>
-
-
-// //1已下单 2已发货 3已完成 4退货中 5完成退货
-// http://49.232.81.174:8080/commodity/getOrderList 两个参数 shop=localstorage.getItem(‘userId’) status=4 获取退货清单
-
-// http://49.232.81.174:8080/commodity/setOrderList 两个参数 orderId status=5 退货成功 拒绝退货的status=3
-
-// 注意：举个例子 你会拿到这样的数据
-// {
-//     "status": 100,
-//     "message": "操作成功",
-//     "data": [
 //         {
 //             "orderId": "3fd24bc19fa44d3ba142c9112b1a0c2b",
 //             "id": "789456",
